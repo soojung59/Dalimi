516cbb1b622d4004eea9b61ea1a2a710d4af4df1
soojung59 <soojungchoi59@gmail.com>
2022-09-19 21:23:41 +0900

    crud완료 시큐리티 진행 중

--- src/main/java/com/sj/dalimi/DalimiApplication.java
+++ src/main/java/com/sj/dalimi/DalimiApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 
 @EnableJpaAuditing
